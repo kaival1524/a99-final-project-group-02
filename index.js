02e10bf73d54802fea6aa78021cf75c9b3db586a
Adam Doyle <adamdoyle630@gmail.com>
2022-11-29 15:34:48 -0500

    Started endpoints

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import Database from 'better-sqlite3';
 const db = new Database('user-data.db');
 db.pragma('journal_mode = WAL');
 
+// Initialize database
 const sqlInit = 'CREATE TABLE entry_log (date INTEGER PRIMARY KEY, intensity TEXT NOT NULL, feeling TEXT NOT NULL)'
 try {
     db.exec(sqlInit);
@@ -15,6 +16,15 @@ try {
 // Initialize app
 const app = express()
 
+// Default endpoint
+app.get('/app', (req, res) => {
+    res.status(200).send('200 OK')
+})
+
+// Post 404 if no endpoint found
+app.get('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND')
+})
 
 app.listen(2000, () => {
     console.log('Server listening on port 2000');
