63e93c52dc088c64b72cb049f835256aac6106e1
Adam Doyle <adamdoyle630@gmail.com>
2022-11-28 22:52:32 -0500

    Created sqlite database

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import express from 'express';
+import Database from 'better-sqlite3';
+
+// Create database
+const db = new Database('user-data.db');
+db.pragma('journal_mode = WAL');
 
 const app = express()
+
+
+app.listen(2000, () => {
+    console.log('Server listening on port 2000');
+});
